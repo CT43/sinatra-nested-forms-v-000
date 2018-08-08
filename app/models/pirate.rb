32ac4ca85d940a0adcb06e8970d59b2b80f49249
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-08 23:09:40 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize(name, weight, height)
+    @name = name 
+    @height = height
+    @weight = weight 
+    @@pirates << self
+  end 
+
+  def self.all 
+    @@pirates 
+  end 
+
+  def self.clear 
+    @@pirates = [] 
+  end 
 end
